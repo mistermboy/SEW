949adc30f2bbe01db026fb35963b1b75886c1e4f
Pablo Menéndez Suárez <pabloyo97@hotmail.com>
2017-11-16 23:19:14 +0100

    use strict

--- public_html/SEW/P6/Ejercicio 3/CalculadoraBasica.js
+++ public_html/SEW/P6/Ejercicio 3/CalculadoraBasica.js
@@ -1,6 +1,7 @@
 // CalculadoraBasica.js
 // Informacion de asignatura
 // Version 1.0. 07/11/2017. Pablo Menéndez Suárez. Universidad de Oviedo
+"use strict";
 class Calculadora {
 
             constructor(){
