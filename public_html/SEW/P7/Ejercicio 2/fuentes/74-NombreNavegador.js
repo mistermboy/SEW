8a4485d8100425d81dcaff087316935bf9d9ccd6
Pablo Menéndez Suárez <pabloyo97@hotmail.com>
2017-11-21 12:12:22 +0100

    P7 -> EJs345

---
+++ public_html/SEW/P7/Ejercicio 2/fuentes/74-NombreNavegador.js
@@ -0,0 +1,5 @@
+/* 74-NombreNavegador.js */
+// Version 1.0. 04/11/2017. Juan Manuel Cueva Lovelle. Universidad de Oviedo
+document.write("<h2>Nombre: ");
+document.write(infoNavegador.nombre);
+document.write("</h2>");
