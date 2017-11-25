99764b09ccb99042036e1d47680d9c82aae0642a
Pablo Menéndez Suárez <pabloyo97@hotmail.com>
2017-11-25 20:30:59 +0100

    encuesta started

---
+++ public_html/SEW/P6/Ejercicio 5/Encuesta.js
@@ -0,0 +1,12 @@
+// Encuesta.js
+// Informacion de asignatura
+// Version 1.0. 16/11/2017. Pablo Menéndez Suárez. Universidad de Oviedo
+"use strict";
+class Encuesta{
+
+         
+        
+}
+
+var e = new Encuesta();
+
